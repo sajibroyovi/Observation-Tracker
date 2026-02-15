7f9da38c50b61b3da61ffb5ae02030a7adb3803a
sajib roy <48487998+sajibroyovi@users.noreply.github.com>
2026-02-15 22:01:29 +0600

    Delete database directory

--- database/migrations/check_schema_role.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../../includes/connection_file.php';
-$result = mysqli_query($conn, "DESCRIBE users");
-while ($row = mysqli_fetch_assoc($result)) {
-    echo $row['Field'] . " - " . $row['Type'] . "\n";
-}
-?>
