5c257fe95a014496a012856d23e14a0c377a53a1
sajibroyovi <sogibroy@gamil.com>
2026-02-17 10:54:06 +0600

    feat: add favicon link to multiple view pages for consistent branding

--- sajib/main/modules/sc/view.php
+++ sajib/main/modules/sc/view.php
@@ -66,6 +66,7 @@ $result = mysqli_stmt_get_result($stmt);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Security Mail</title>
+    <link rel="icon" type="image/png" href="<?php echo ASSETS_URL; ?>/uploads/bkash_logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <link rel="stylesheet" href="<?= ASSETS_URL ?>/css/style.css">
--- sajib/main/modules/users/manage.php
+++ sajib/main/modules/users/manage.php
@@ -21,6 +21,7 @@ $result = $stmt ? mysqli_stmt_get_result($stmt) : false;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Management | Shift Handover</title>
+     <link rel="icon" type="image/png" href="<?php echo ASSETS_URL; ?>/uploads/bkash_logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <link rel="stylesheet" href="<?= ASSETS_URL ?>/css/style.css">
